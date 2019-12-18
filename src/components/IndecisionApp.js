c586097df9a320c8b5b66ef44b45fcd11a13a166
Yiting <shenyiting2018@gmail.com>
2019-12-17 17:08:35 -0800

    section 7

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -3,13 +3,18 @@ import AddOption from './AddOptions';
 import Action from './Action';
 import Header from './Header';
 import Options from './Options';
+import OptionModal from './OptionModal';
 
 export default class IndecisionApp extends React.Component {
     state = {
-        options: []
+        options: [],
+        selectedOption: undefined
     };
     handleDeleteOptions = () => {
         this.setState(() => ({ options: [] }));
+    };
+    handleClearSelectedOption = () => {
+        this.setState(() => ({ selectedOption: undefined }));
     };
     handleDeleteOption = (optionToRemove) => {
         this.setState((prevState) => ({
@@ -19,7 +24,9 @@ export default class IndecisionApp extends React.Component {
     handlePick = () => {
         const randomNum = Math.floor(Math.random() * this.state.options.length);
         const option = this.state.options[randomNum];
-        alert(option);
+        this.setState(() => ({
+            selectedOption: option
+        }));
     };
 
     handleAddOption = (option) => {
@@ -86,6 +93,10 @@ export default class IndecisionApp extends React.Component {
                 />
                 <AddOption 
                     handleAddOption={this.handleAddOption}
+                />
+                <OptionModal 
+                    selectedOption={this.state.selectedOption}
+                    handleClearSelectedOption={this.handleClearSelectedOption}
                 />
             </div>
         );
--- src/components/Option.js
+++ src/components/Option.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-const Option = (props) => {
-    return (
-        <div>
-            {props.optionTest}
-            <button 
-                onClick={(e) => {
-                    props.handleDeleteOption(props.optionTest);
-                }}
-            >
-                remove
-            </button>
-        </div>
-    );
-};
+const Option = (props) => (
+    <div>
+        {props.optionTest}
+        <button 
+            onClick={(e) => {
+                props.handleDeleteOption(props.optionTest);
+            }}
+        >
+            remove
+        </button>
+    </div>
+);
 
 export default Option;
