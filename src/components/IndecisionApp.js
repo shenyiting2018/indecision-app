eab99a808fb768a07d179e3d090629dcf43a2641
Yiting <shenyiting2018@gmail.com>
2019-12-19 17:44:37 -0800

    section 8

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -69,19 +69,24 @@ export default class IndecisionApp extends React.Component {
 
         return (
             <div>
-                <Header subtitle={subtitle} />
-                <Action 
+                <Header subtitle={subtitle} />                                         
+                <div className="container">
+                    <Action 
                     hasOptions={this.state.options.length > 0}
                     handlePick={this.handlePick}
-                />
-                <Options 
-                    options={this.state.options}
-                    handleDeleteOptions={this.handleDeleteOptions}
-                    handleDeleteOption={this.handleDeleteOption}
-                />
-                <AddOption 
-                    handleAddOption={this.handleAddOption}
-                />
+                    />
+                    <div className="widget">
+                        <Options 
+                            options={this.state.options}
+                            handleDeleteOptions={this.handleDeleteOptions}
+                            handleDeleteOption={this.handleDeleteOption}
+                        />
+                        <AddOption 
+                            handleAddOption={this.handleAddOption}
+                        />
+                    </div>                   
+                </div>
+                
                 <OptionModal 
                     selectedOption={this.state.selectedOption}
                     handleClearSelectedOption={this.handleClearSelectedOption}
--- src/components/Option.js
+++ src/components/Option.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Option = (props) => (
-    <div>
-        {props.optionTest}
+    <div className="option">
+        <p className="option__text">{props.count}. {props.optionTest}</p>
         <button 
+            className="button button--link"
             onClick={(e) => {
                 props.handleDeleteOption(props.optionTest);
             }}
